d6d85bfadf335dceaedab1b94654e5e6ea066993
wajihafarhan <122856401+wajihafarhan@users.noreply.github.com>
2024-03-27 10:29:11 +0500

    Add files via upload

---
+++ encapsulation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+    public class bank
+    {
+        public int acc = 2000;
+
+        public int number
+        {
+            set
+            {
+                acc = value;
+            }
+            get
+            {
+                return acc;
+            }
+        }
+
+    }
+    //public class bank
+    //{
+    //    int accblnc = 250000;
+    //    string name = "wajiha";
+    //    int accno = 123456;
+    //    public void SetBlnc(int name, int account ,int accountNO)
+    //    {
+    //        name = name;
+    //        accblnc = account;
+    //        accno = accountNO;
+
+    //    }
+    //    public void GetBlnc()
+    //    {
+    //        Console.WriteLine($"your ACCOUNT NAME is {name}");
+    //        Console.WriteLine($"your ACCOUNT NO is {accno}");
+    //        Console.WriteLine($"your ACCOUNT blnc is {accblnc}");
+
+    //    }
+    //}
+    public class encapsulation
+    {
+        static void Main(string[] args)
+        {
+            bank acc2 = new bank();
+            acc2.acc = 12000;
+            Console.WriteLine(acc2.number);
+            //bank acc = new();
+
+            ////acc.SetBlnc(1000);
+            //acc.GetBlnc();
+        }
+
+    }
+}
+
